cb28dfaee4a3893951515dba3f3f88cf5c325521
Patrick <pcrussel@ucsc.edu>
2016-01-13 19:40:04 -0800

    Added Pause Menu Functionality
    The loading of levels works*
    
    *Directional lighting seems to fail upon loading, as does the menu
    itself.

--- Assets/Scripts/LoadOnClick.cs
+++ Assets/Scripts/LoadOnClick.cs
@@ -4,8 +4,9 @@ using System.Collections;
 
 public class LoadOnClick : MonoBehaviour {
 
-	public void LoadScene(int levelNum)
+	public void LoadScene(string level)
     {
-        SceneManager.LoadScene(levelNum);
+        Debug.Log("Loading!");
+        SceneManager.LoadScene(level);
     }
 }
