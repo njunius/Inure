bf55388804848413d8daa53c31beb335ccba4e90
Nick Junius <njunius@ucsc.edu>
2016-01-14 19:41:56 -0800

    Added basic HUD elements and shield UI logic
    Added Shield bar with control logic
    Added Bomb Circular gauge
    Added Sample Armor Points Gauge

---
+++ Assets/Scripts/Shield and HUD/BombController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombController : MonoBehaviour {
+
+    public bool isPlanted;
+    public int currBombCharge;
+    public int maxBombCharge;
+
+    public Transform playerLocation;
+
+	// Use this for initialization
+	void Start () {
+
+        isPlanted = false;
+        currBombCharge = 0;
+        maxBombCharge = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isPlanted)
+        {
+            transform.position = playerLocation.position;
+        }
+	}
+
+    /*
+    * pre: newCharge is a positive number
+    * post: currBombCharge += newCharge and currBombCharge <= maxBombCharge
+    */
+    public void chargeBomb(int newCharge)
+    {
+        currBombCharge += newCharge;
+        if (currBombCharge > maxBombCharge)
+            currBombCharge = maxBombCharge;
+    }
+}
