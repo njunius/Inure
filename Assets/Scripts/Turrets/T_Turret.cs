ccf26c2457df3ff279a42cf3cfcde27a3a87cca7
JosephBernay <josephbernay@gmail.com>
2016-01-14 17:09:04 -0800

    Re-finished PointTurret
    Everything works again

---
+++ Assets/Scripts/Turrets/T_Turret.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class T_Turret : SimpleTurret {
+
+	T_Turret() {
+		bulletVel = Velocity.Extreme;
+		bulletColor = Color.cyan; //FIND HEX FOR TURQUOISE
+		fireRate = RateOfFire.High;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Turrets/Turret.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * Turret.cs
+ * 
+ * Creates or defines specifics common among all children of Turret:
+ * - Velocity and rate-of-fire keys
+ * - Orientation of object
+ * - Velocity, color, rate-of-fire, and despawn distance of bullets
+ * - List of barrels for turret
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class Turret : MonoBehaviour {
+
+	//higher number = higher velocity
+	protected enum Velocity {Low = 0, Medium = 1, High = 0, Extreme = 20};
+	//lower number = higher rate
+	protected enum RateOfFire {Low = 0, Medium = 5, High = 0, Extreme = 0};
+
+	protected Vector3 turretPos;
+	protected Quaternion turretRot = new Quaternion(0, 0, 0, 0);
+	protected Velocity bulletVel;
+	protected Vector4 bulletColor;
+	protected TurretBarrel[] barrelList;
+	protected RateOfFire fireRate;
+	protected float fireRateMultiplier = 0.1f;
+	protected Vector3 endOfTurret;
+	protected float despawnDist; //UNDEFINED
+
+	public GameObject bulletPrefab;
+
+	// Use this for initialization
+	void Start () {
+		turretPos = gameObject.GetComponent<Rigidbody>().position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
