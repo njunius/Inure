c4bfc0be083439f3112b28884ff47947e0cf20d4
Patrick <pcrussel@ucsc.edu>
2016-01-19 17:19:31 -0800

    Added initial function
    Block moves towards one node

---
+++ Assets/Scripts/objectMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class objectMove : MonoBehaviour {
+	public GameObject node1, node2;
+	public float speed = 1;
+	private float step = 0;
+	public Vector3 startPOS, endPOS;
+
+	// Use this for initialization
+	void Start () {
+		startPOS = node1.transform.position;
+		endPOS = node2.transform.position;
+	}
+	
+	//Move the cube towards either the Start or End nodes
+	void FixedUpdate () {
+		transform.position = Vector3.MoveTowards(transform.position, node2.transform.position, speed);
+	}
+}
