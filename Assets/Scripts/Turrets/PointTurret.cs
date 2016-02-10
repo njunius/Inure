e6c12c86ddd7f3c5dd8680d3d0d6ab2d25e64108
Patrick <pcrussel@ucsc.edu>
2016-02-09 16:52:51 -0800

    Edited Fire Rate and Tagged Levels
    Also added global lighting just for testing

--- Assets/Scripts/Turrets/PointTurret.cs
+++ Assets/Scripts/Turrets/PointTurret.cs
@@ -18,7 +18,7 @@ public class PointTurret : SimpleTurret {
 	void Start () {
 		bulletVel = Velocity.Extreme;
 		bulletColor = Color.blue;
-		fireRate = RateOfFire.Low;
+		fireRate = RateOfFire.Medium;
 		barrelList = new TurretBarrel[1];
 		barrelList [0] = new TurretBarrel ((float)bulletPrefab.GetComponent<Renderer>().bounds.size.x, 
 											(int)bulletVel);
