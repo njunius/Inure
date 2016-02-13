a087b355ae5b10eb2304fdf7e5f85dcb7c8e87c4
Patrick <pcrussel@ucsc.edu>
2016-02-12 17:32:52 -0800

    Initial Push
    Added a test scene, and the blank objects

---
+++ Assets/turretTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turretTrigger : MonoBehaviour {
+
+	public GameObject[] turretsInRoom = new GameObject[2];
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < turretsInRoom.Length; i++) {
+			turretsInRoom [i].SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
