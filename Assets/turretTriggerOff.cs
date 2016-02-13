bb2d7774d27d234915d4e331ae07f9287c820b60
Patrick <pcrussel@ucsc.edu>
2016-02-12 17:47:03 -0800

    TurretTrigger Script Works
    Need to now make turrets turn OFF.

--- Assets/turretTriggerOff.cs
+++ Assets/turretTriggerOff.cs
@@ -1,29 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class turretTrigger : MonoBehaviour {
+public class turretTriggerOff : MonoBehaviour {
 
 	public GameObject[] turretsInRoom = new GameObject[2];
 
-	// First, turn off turrets
-	void Start () {
+	// Use this for initialization
+	/*void Start () {
 		for (int i = 0; i < turretsInRoom.Length; i++) {
 			turretsInRoom [i].SetActive(false);
 		}
-	}
+	}*/
 
 	void OnTriggerEnter(Collider other){
 		if(other.CompareTag("Player Collider")){
-			//Debug.Log ("Player is in");
+			
 			for (int i = 0; i < turretsInRoom.Length; i++) {
-				//Debug.Log ("Activating Turret");
-				turretsInRoom [i].SetActive(true);
+				
+				turretsInRoom [i].SetActive(false);
 			}
 		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
