810c9222e8570a0f228555f7c90764e5ba3b12a8
Patrick <pcrussel@ucsc.edu>
2016-02-15 22:21:56 -0800

    Initial Push
    Added the checkpoint, and the scripts to store the data.

---
+++ Assets/LastCheckpoint.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LastCheckpoint : MonoBehaviour {
+
+	public Vector3 savedPOS;
+	public float shieldCharge;
+	public int bombCharge;
+	public float hullHealth;
+
+	private PlayerController cont;
+	private BombController bomb;
+
+	// Use this for initialization
+	void Start () {
+		savedPOS = gameObject.transform.position;
+		cont = gameObject.GetComponent<PlayerController> ();
+		bomb = GameObject.FindGameObjectWithTag("Bomb").GetComponent<BombController>();
+		bombCharge = bomb.currBombCharge;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
