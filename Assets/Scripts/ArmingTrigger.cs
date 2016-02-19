7a22c604138c20b1209803b7f7a4644a7b58f7d0
Yasha Taylor <YashaTaylor@gmail.com>
2016-02-18 20:22:26 -0800

    Bomb now detaches with capsule collider

---
+++ Assets/Scripts/ArmingTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ArmingTrigger : MonoBehaviour {
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        if (other.gameObject.tag == "Bomb")
+        {
+           
+            other.gameObject.GetComponent<BombController>().armBomb();
+        }
+    }
+}
