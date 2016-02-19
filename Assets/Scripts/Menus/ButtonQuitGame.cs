c5c77fe536853b1d17161eddeaab221a1b27ee2b
Nick Junius <njunius@ucsc.edu>
2016-02-19 03:32:19 -0800

    Added Main Menu and Briefing Screen

---
+++ Assets/Scripts/Menus/ButtonQuitGame.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ButtonQuitGame : MonoBehaviour, IPointerDownHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	public void OnPointerDown (PointerEventData eventData) {
+        Application.Quit();	
+	}
+}
