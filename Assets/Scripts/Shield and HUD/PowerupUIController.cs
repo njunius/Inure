755febd907fb16e0ec0bdb2c29785060e2f0c0f7
Nick Junius <njunius@ucsc.edu>
2016-02-25 13:47:48 -0800

    Added UI to display current powerups
    Player can now see which powerup they have equipped.
    
    Also added an accessor to the player to get the index of the current
    powerup.
    
    Moved UI sprites into Resources folder to allow the use of
    Resources.load to dynamically get UI elements for the powerup display.

---
+++ Assets/Scripts/Shield and HUD/PowerupUIController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PowerupUIController : MonoBehaviour {
+
+    public Sprite[] powerupSprites;
+    private Image[] powerupGauges;
+    private PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+        powerupSprites = new Sprite[] { Resources.Load<Sprite>("UI Sprites/EmptyPowerup"), Resources.Load<Sprite>("UI Sprites/EMPPlaceHolder"),
+                                        Resources.Load<Sprite>("UI Sprites/ShockwavePlaceHolder"), Resources.Load<Sprite>("UI Sprites/TimeSlowPlaceholder")};
+
+
+        GameObject[] temp = GameObject.FindGameObjectsWithTag("Powerup Gauge");
+        powerupGauges = new Image[temp.Length];
+
+        for (int i = 0; i < powerupGauges.Length; ++i)
+        {
+            powerupGauges[i] = temp[i].GetComponent<Image>();
+        }
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for (int i = 0; i < powerupGauges.Length; ++i)
+        {
+            powerupGauges[i].sprite = powerupSprites[player.getPowerupIndex()];
+        }
+    }
+}
