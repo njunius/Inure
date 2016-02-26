d3f9f4998458b7d18bc66fbeb4feefc6a652e4ca
Nick Junius <njunius@ucsc.edu>
2016-02-26 00:07:44 -0800

    Keys can now be rebound
    The player can now select a command and rebind the key to the next
    button they press.
    
    Note: They can only bind keyboard inputs currently.
    
    Note: There are no alternates, each command can have exactly 1 input
    currently.
    
    Note: The selected field will not appear selected.

--- Assets/Scripts/Menus/CustomKeyController.cs
+++ Assets/Scripts/Menus/CustomKeyController.cs
@@ -1,39 +1,76 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using System.Collections;
 using System.Collections.Generic;
 
-public class CustomKeyController : MonoBehaviour {
+public class CustomKeyController : MonoBehaviour, IPointerDownHandler
+{
 
     private InputManager inputs;
     private Dictionary<string, InputBinding> inputBindings;
-    public Text currentKey;
+    private Selectable button;
     private string key;
-    private bool initialized;
+    private bool selected;
+    private Event controlBindEvent;
 
+    public EventSystem events;
+    public Text currentKey;
     public string command;
     public bool positiveDirection;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+
         inputs = GameObject.FindGameObjectWithTag("GameController").GetComponent<InputManager>();
-
         currentKey = gameObject.GetComponentInChildren<Text>();
+        button = GetComponent<Selectable>();
 
-        initialized = false;
+        selected = false;
 
     }
-	
-	// Update is called once per frame
-	void Update () {
 
-        if(inputBindings == null)
+    void OnGUI()
+    {
+
+        controlBindEvent = Event.current;
+
+        if (selected && controlBindEvent != null)
+        {
+
+            if (controlBindEvent.isKey)
+            {
+
+                key = controlBindEvent.keyCode.ToString().ToLower();
+
+                if ((inputBindings[command].bidirectional && positiveDirection) || !inputBindings[command].bidirectional)
+                {
+                    inputBindings[command].posAxis = key;
+                }
+                else if (inputBindings[command].bidirectional && !positiveDirection)
+                {
+                    inputBindings[command].negAxis = key;
+                }
+
+
+                selected = false;
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (inputBindings == null)
         {
             inputBindings = inputs.getInputBindings();
 
         }
 
-        if (inputBindings != null && !initialized)
+        if (inputBindings != null)
         {
             if ((inputBindings[command].bidirectional && positiveDirection) || !inputBindings[command].bidirectional)
             {
@@ -44,9 +81,14 @@ public class CustomKeyController : MonoBehaviour {
                 key = inputBindings[command].negAxis;
             }
 
-            initialized = true;
         }
 
         currentKey.text = key.ToUpper();
-	}
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        selected = true;
+        return;
+    }
 }
