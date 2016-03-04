0d1520a47431f27a1dff4f3dc68849351dfa08ea
Patrick <pcrussel@ucsc.edu>
2016-03-03 22:12:45 -0800

    Added Lighting
    Lighting Added

--- Assets/Scripts/fakeOutTrigger.cs
+++ Assets/Scripts/fakeOutTrigger.cs
@@ -8,10 +8,12 @@ public class fakeOutTrigger : MonoBehaviour {
 
 	private GameObject door;
 	private bool slam = false;
+	private GameObject light;
 
 	// Use this for initialization
 	void Start () {
 		door = GameObject.FindGameObjectWithTag("Gate");
+		light = door.transform.GetChild(1).gameObject;
 	}
 
 	void OnTriggerEnter (Collider other) {
@@ -24,6 +26,11 @@ public class fakeOutTrigger : MonoBehaviour {
 	void FixedUpdate () {
 		if(slam && door.transform.position.z < maximum){
 			door.transform.Translate(0, 0, speed * Time.deltaTime);
+		}
+
+		if(door.transform.position.z >= maximum){
+			slam = false;
+			light.GetComponent<Light>().enabled = true;
 		}
 	}
 }
