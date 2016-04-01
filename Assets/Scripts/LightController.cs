3b2eb76ee5c0a290cae4122a3449a06eddadce97
Yasha N Taylor <YashaTaylor@gmail.com>
2016-04-01 13:30:23 -0700

    Added Light Controller

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightController : MonoBehaviour
+{
+    public float duration = 1.0F;
+    public Light lt;
+    void Start()
+    {
+        lt = GetComponent<Light>();
+        GameObject[] allLights = GameObject.FindGameObjectsWithTag("GLight");
+    }
+    void Update()
+    {
+        float phi = Time.time / duration * 0.6f * Mathf.PI;
+        float amplitude = Mathf.Cos(phi) * 0.9F + 0.9F;
+        lt.intensity = amplitude;
+    }
+}
+
+/*foreach (GameObject i in "GLight")
+        {
+            i.SetActive(false);
+        }
+*/
