3143cde1a1437ea9454cd1a29be056511afdd9de
Patrick <pcrussel@ucsc.edu>
2016-04-06 11:26:43 -0700

    Added Orbit Script
    Things can now orbit other things

---
+++ Assets/Scripts/Orbit.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+
+public class Orbit : MonoBehaviour {
+	public float rotation_rate;
+	public GameObject orbit_point;
+	void fixedUpdate()
+	{
+		//gameObject.transform.Rotate(rotation_rate * Time.deltaTime);
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		gameObject.transform.RotateAround(orbit_point.transform.position, Vector3.up, rotation_rate * Time.deltaTime);
+	}
+}
