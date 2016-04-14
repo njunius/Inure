ea741fd981d6b06af58a55db44f1b45eb79395db
Yasha Taylor <YashaTaylor@gmail.com>
2016-04-13 22:04:21 -0700

    Bomb now detaches properly

--- Assets/Scripts/BombDetach.cs
+++ Assets/Scripts/BombDetach.cs
@@ -3,20 +3,30 @@ using System.Collections;
 
 public class BombDetach : MonoBehaviour
 {
-    GameObject core;
-	// Use this for initialization
-	void Start ()
+    // GameObject bomb;
+    bool detached = false;
+    Vector3 offset = new Vector3 (0, -1.139f, -4.079f);
+    void OnTriggerEnter(Collider col)
     {
-        core = GameObject.FindGameObjectWithTag("Core");
+        if (col.gameObject.name == "ReactorDetail")
+        {
+            detached = true;
+            Debug.Log("Bomb is detached!");
+            col.transform.parent = null;
+        }
+    }
+    // Use this for initialization
+    void Start ()
+    {
+        // bomb = GameObject.FindGameObjectWithTag("Bomb");
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        //int distance = Vector3.Distance(core, transform.position);
-        if (distance < 10)
+        if (!detached)
         {
-            transform.parent = null;
+            transform.position = transform.parent.position + offset;
         }
     }
 }
