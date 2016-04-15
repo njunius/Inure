69784d078fced7bdd8d3b6ed9a6dc0dc5ce5121b
Yasha Taylor <YashaTaylor@gmail.com>
2016-04-14 18:40:01 -0700

    getter countdown timer, win/losegame trigger
    Made a getter for the countdown timer and escape tunnel trigger for win
    game / die with explosion.

--- Assets/Scripts/BombDetach.cs
+++ Assets/Scripts/BombDetach.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class BombDetach : MonoBehaviour
 {
     bool detached = false;
-    float count_down = 10;
+    float count_down = 60;
+    bool escaped = false;
+
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.name == "ReactorDetail")
@@ -13,6 +15,13 @@ public class BombDetach : MonoBehaviour
             detached = true;
         }
     }
+
+    // retrieve the value of the current time
+    public float get_countdown()
+    {
+        return count_down;
+    }
+
     // Use this for initialization
     void Start()
     {
@@ -25,11 +34,20 @@ public class BombDetach : MonoBehaviour
         if (detached)
         {
             count_down -= Time.deltaTime;
-            //Debug.Log(count_down);
-            if (count_down <= 0)
+            Debug.Log(count_down);
+            if (count_down <= 0 && !escaped)
             {
                 Debug.Log("Game Over!");
             }
+            else if(count_down <= 0 && escaped)
+            {
+                Debug.Log("You Win!");
+            }
         }
+    }
+
+    public void set_escape(bool escape_val)
+    {
+        escaped = escape_val;
     }
 }
---
+++ Assets/Scripts/EscapeGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeGame : MonoBehaviour
+{
+    BombDetach bd = GameObject.FindGameObjectWithTag("Bomb").GetComponent<BombDetach>();
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "New Player")
+        {
+            bd.set_escape(true);
+        }
+    }
+}
