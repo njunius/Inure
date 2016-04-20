d737fc94a4afa5c0a10a6e0e9989af5565add0e2
Yasha Taylor <YashaTaylor@gmail.com>
2016-04-19 21:58:59 -0700

    Added Screenshake when countdown starts and entrance becomes sealed

---
+++ Assets/Scripts/MoveEscapeBlocker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveEscapeBlocker : MonoBehaviour
+{
+    BombDetach bd;
+    public float speed;
+    public Transform desiredSpot;
+	// Use this for initialization
+	void Start ()
+    {
+        bd = GameObject.FindGameObjectWithTag("Bomb").GetComponent<BombDetach>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(bd.detached)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, desiredSpot.transform.position, step);
+        }
+	}
+}
