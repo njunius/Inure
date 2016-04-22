2c903822ccce2cccd25285e778bb55c8ae5bbea2
Patrick <pcrussel@ucsc.edu>
2016-04-22 12:17:19 -0700

    Created Manager Script
    It'll track when shield bits are destroyed and spawn turrets whenever
    the player makes progress.

---
+++ Assets/BossManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossManager : MonoBehaviour {
+	public GameObject reactor;
+	public GameObject[] shieldPieces;
+	public int turretInterval = 4;
+
+	private int shieldCount = 32;
+	// Use this for initialization
+	void Start () {
+		shieldCount = shieldPieces.Length;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
