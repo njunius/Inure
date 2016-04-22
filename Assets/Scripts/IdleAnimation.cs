3ca73d5f0f059d3333a11cecbae9810eeed9924e
Yasha Taylor <YashaTaylor@gmail.com>
2016-04-22 10:56:11 -0700

    Added in animation when player becomes Idol
    Basic Animation, will polish and smoothen(Lerp instead of instant) in
    next update

---
+++ Assets/Scripts/IdleAnimation.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IdleAnimation : MonoBehaviour
+{
+    int counter = 0;
+    bool tilt = false;
+    bool second = false;
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!Input.GetKey("w") && !Input.GetKey("a") && !Input.GetKey("s") && !Input.GetKey("d") && !Input.GetKey("space") && !Input.GetKey(KeyCode.LeftShift) && !Input.GetKey("q") && !Input.GetKey("e"))
+        {
+            counter++;
+            //Debug.Log("staying still!");
+            if (counter == 20 && tilt == false && second == false)
+            {
+                transform.Rotate(0, 0, -1);
+                tilt = true;
+                counter = 0;
+                second = true;
+            }
+            if (counter == 20 && tilt == false && second == true)
+            {
+                transform.Rotate(0, 0, -4);
+                tilt = true;
+                counter = 0;
+            }
+            if (counter == 20 && tilt == true && second == true)
+            {
+                transform.Rotate(0, 0, 4);
+                tilt = false;
+                counter = 0;
+            }
+        }
+    }
+}
