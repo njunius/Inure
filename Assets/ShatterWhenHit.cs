1f9d9f989110dda46db693dfa5ef641b01b2c636
Patrick <pcrussel@ucsc.edu>
2016-04-21 19:55:58 -0700

    Created Prefab of Inner Reactor SHATTERED
    Hopefully I can just spawn this when the player shoots the shell.

---
+++ Assets/ShatterWhenHit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShatterWhenHit : MonoBehaviour {
+	public GameObject shatterInto;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	//when the player shoots the thing, create the child object to shatter
+	void OnCollisionEnter(Collision collision) {
+		
+	}
+}
