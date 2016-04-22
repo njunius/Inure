e55bb83d53addc5d6e4c080c0077d35b817bb9eb
Patrick <pcrussel@ucsc.edu>
2016-04-22 12:05:49 -0700

    Added HP to Reactor Shield
    Shield will now only hilariously explode when shot multiple times.

--- Assets/Scripts/Reactor/ShatterWhenHit.cs
+++ Assets/Scripts/Reactor/ShatterWhenHit.cs
@@ -3,20 +3,25 @@ using System.Collections;
 
 public class ShatterWhenHit : MonoBehaviour {
 	public GameObject shatterInto;
+	public int hitPoints = 4;
+
+	private int hpInternal = 0;
 	// Use this for initialization
 	void Start () {
-	
+		hpInternal = hitPoints;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-	//when the player shoots the thing, create the child object to shatter
-	void OnCollisionEnter(Collision collision) {
-		if(collision.collider.tag == "Player Projectile"){
+		if(hpInternal <= 0){
 			Instantiate(shatterInto, transform.position, transform.rotation);
 			Destroy(gameObject);
+		}
+	}
+	//when the player shoots the thing, reduce HP by 1
+	void OnCollisionEnter(Collision collision) {
+		if(collision.collider.tag == "Player Projectile"){
+			hpInternal--;
 		}
 	}
 }
