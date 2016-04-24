a66fe62cb6f316383d3a88df3897c7f8923ab4b3
Yasha Taylor <YashaTaylor@gmail.com>
2016-04-24 14:09:29 -0700

    Added a door to escape tunnel and only destoryable once bomb is detached

---
+++ Assets/Scripts/Reactor/EscapeTunnelShatter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EscapeTunnelShatter : MonoBehaviour
+{
+    public GameObject shatterInto;
+    public int hitPoints = 4;
+    BombDetach bd;
+
+    private int hpInternal = 0;
+    // Use this for initialization
+    void Start()
+    {
+        hpInternal = hitPoints;
+        bd = GameObject.FindGameObjectWithTag("Bomb").GetComponent<BombDetach>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hpInternal <= 0)
+        {
+            Instantiate(shatterInto, transform.position, transform.rotation);
+            Destroy(gameObject);
+        }
+    }
+    //when the player shoots the thing, reduce HP by 1
+    void OnCollisionEnter(Collision collision)
+    {
+        if (bd.detached == true)
+        {
+            if (collision.collider.tag == "Player Projectile")
+            {
+                hpInternal--;
+                Destroy(collision.gameObject);
+            }
+        }
+    }
+}
