d9f62190800e19e25a843e6325187f1d45099cb1
Nick Junius <njunius@ucsc.edu>
2016-04-25 02:43:58 -0700

    New Pause Screen and Logic
    The pause screen has a new layout and updated assets.
    
    The logic to pause the game has been mostly moved into a new script on
    the player prefab (pause controller).
    
    Pausing the game now stops most player logic from happening.
    
    New Pause screen in scenes.

---
+++ Assets/Scripts/Menus/DeathNotificationController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DeathNotificationController : MonoBehaviour {
+
+    private PlayerController player;
+    private Image background;
+    private Text deathText;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+
+        background = GetComponent<Image>();
+        deathText = gameObject.GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (player.isDead())
+        {
+            background.enabled = true;
+            deathText.enabled = true;
+        }
+        else
+        {
+            background.enabled = false;
+            deathText.enabled = false;
+        }
+	}
+}
---
+++ Assets/Scripts/Menus/PauseController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PauseController : MonoBehaviour {
+
+    private Canvas pauseOverlay;
+    private Canvas settingsOverlay;
+    private PlayerController player;
+
+    public InputManager im;
+
+    // Use this for initialization
+    void Start () {
+
+        pauseOverlay = GameObject.FindGameObjectWithTag("Pause Overlay").GetComponent<Canvas>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        settingsOverlay = GameObject.FindGameObjectWithTag("Settings Screen").GetComponent<Canvas>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (im.getInputDown("Pause") && !player.paused)
+        {
+            player.paused = !player.paused;
+            Time.timeScale = 0;
+            pauseOverlay.enabled = true;
+            Cursor.visible = true;
+        }
+        else if (im.getInputDown("Pause") && player.paused)
+        {
+            if (!settingsOverlay.enabled)
+            {
+                player.paused = !player.paused;
+                Time.timeScale = 1;
+                pauseOverlay.enabled = false;
+                Cursor.visible = false;
+            }
+            else if (settingsOverlay.enabled)
+            {
+                settingsOverlay.enabled = false;
+            }
+        }
+        if (Cursor.visible && !player.paused && !player.isDead())
+            Cursor.visible = false;
+    }
+
+}
