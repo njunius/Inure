03a456f53916b8da89e0a9a2db37f0ce21c9c823
Nick Junius <njunius@ucsc.edu>
2016-05-01 03:52:55 -0700

    New assets and improved layout
    The settings screen has new buttons to allow for a smoother
    "cursor-like" effect when mousing over keybindings.
    
    Updated text positions on buttons and made text uniform size across each
    size and type of button.
    
    New Layout for pause screen.
    
    NOTE: The "Mission Failed" background image needs a new asset so it
    doesn't look like a button.

---
+++ Assets/Scripts/Menus/ButtonDeselect.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ButtonDeselect : MonoBehaviour, IPointerDownHandler {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+    }
+
+}
