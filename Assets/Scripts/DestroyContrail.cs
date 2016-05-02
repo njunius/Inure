0bf830974936c10ed11847eda311b6d165d5ea05
JosephBernay <josephbernay@gmail.com>
2016-05-01 23:43:18 -0700

    Completely Unrealistic Contrails
    Contrails are now produced from the back of the wings during forward
    movement.

---
+++ Assets/Scripts/DestroyContrail.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyContrail : MonoBehaviour {
+
+	private bool wasGivenParent = false;
+	private bool isTriggered = false;
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+		time = GetComponent<TrailRenderer> ().time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (wasGivenParent && !isTriggered && transform.parent == null) {
+			isTriggered = true;
+		} else if (isTriggered) {
+			Die ();
+		}
+	}
+
+	public void GiveParent (Transform parent) {
+		transform.parent = parent;
+		wasGivenParent = true;
+	}
+
+	private void Die () {
+		transform.parent = transform;
+
+		Destroy (gameObject, time);
+	}
+}
