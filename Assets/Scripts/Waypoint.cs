dfd618b30f0198610f628395cc7a23d6226ed785
Joe Rossi <joethered@gmail.com>
2016-05-03 16:04:28 -0700

    Changed Waypoint behavior
    They now use box colliders which the player will remain perpendicular to as long as the waypoint is directly in front of the player.  Otherwise the player will rotate around the last perpendicular point.  This ought to reduce the chances of the player flying around a waypoint.

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -4,13 +4,76 @@ using System.Collections;
 public class Waypoint : MonoBehaviour {
 
     public GameObject NextPoint;
+    public GameObject PrevPoint;
+    public bool isBox = true;
+
+    public bool active = false;
+
+    public GameObject target;
+    private Vector3 prevDir;
+
+    void Start()
+    {
+        if (isBox)
+        {
+            target = transform.FindChild("Waypoint Target").gameObject;
+        }
+    }
+
+    void Update()
+    {
+        if (PrevPoint != null)
+        {
+            transform.LookAt(PrevPoint.transform);
+            target.transform.rotation = transform.rotation;
+            prevDir = transform.position - PrevPoint.transform.position;
+        }
+
+        if (active && isBox)
+        {
+            Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+            //Vector3 waypointDirection = -1 * transform.eulerAngles;
+
+            Debug.DrawRay(player.position, prevDir, Color.yellow);
+            int layer = 1 << 14;
+            RaycastHit hit;
+            if (Physics.Raycast(player.position, prevDir, out hit, 1000.0f, layer))
+            {
+                if (hit.transform.gameObject.GetComponent<Waypoint>().active)
+                {
+                    target.transform.position = hit.point;
+                }
+                
+            }
+
+        }
+        
+    }
+
+    public GameObject getTarget()
+    {
+        return target;
+    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player Turret Trigger"))
         {
             PlayerController pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
-            pc.lockOnTarget = NextPoint;
+            Waypoint next = NextPoint.GetComponent<Waypoint>();
+            if (next.isBox)
+            {
+                
+                pc.lockOnTarget = next.getTarget();
+                next.active = true;
+
+            }
+            else
+            {
+                pc.lockOnTarget = NextPoint;
+            }
+            active = false;
+            
             pc.targetLocked = true;
         }
     }
