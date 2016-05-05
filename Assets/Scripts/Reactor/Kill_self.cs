e322c6f14e32f9f3238e4236d7b8ac5f1d30500b
Patrick <pcrussel@ucsc.edu>
2016-05-05 08:45:59 -0700

    Added Kill_Self Script
    Fairly simply 20 lines of code. It counts down from a set time, then
    destroys whatever the script's attached to. I put it on all the
    shattered prefabs, so now they won't clog up the physics budget.

---
+++ Assets/Scripts/Reactor/Kill_self.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Kill_self : MonoBehaviour {
+	public float lifeTime = 4; // The ammount of time left before self destruction in seconds
+
+	private float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+
+		if(timer >= lifeTime)
+			Destroy(gameObject);
+	}
+}
