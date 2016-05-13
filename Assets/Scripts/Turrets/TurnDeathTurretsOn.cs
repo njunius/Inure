be88f20202620b784a28083546b27c27b1c7d4c7
Yasha N Taylor <YashaTaylor@gmail.com>
2016-05-12 18:10:17 -0700

    Death turrets now obliterate player after exiting Juggernaut's Shield

---
+++ Assets/Scripts/Turrets/TurnDeathTurretsOn.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TurnDeathTurretsOn : MonoBehaviour {
+
+	public List<GameObject> turretList = new List<GameObject> ();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.gameObject.CompareTag ("Player Turret Trigger")) {
+			toggleTurrets ();
+		}
+	}
+
+	void toggleTurrets () {
+		for (int numTurret = 0; numTurret < turretList.Count; ++numTurret) {
+			turretList [numTurret].GetComponent<Turret> ().TurnOn ();
+		}
+	}
+}
