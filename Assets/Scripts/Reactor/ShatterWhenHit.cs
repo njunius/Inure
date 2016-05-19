3759bffff53617be3d7f4041fbd105dd6cdae220
Nick Junius <njunius@ucsc.edu>
2016-05-18 23:15:24 -0700

    Made Door HP Indicators more flexible
    Door HP indicators now use a system similar to the bomb gauge and are
    the same color as the bomb gauge.
    
    Door HP indicators also maintain their circular shape.
    NOTE: MAKE SURE YOU EDIT THE DOOR SOLID OBJECT AND NOT THE HOLDER OBJECT
    IF YOU MESS WITH SCALE.
    
    Fixed out of bounds index on the armor controller if the added HP would
    overflow the maximum allowed HP.

--- Assets/Scripts/Reactor/ShatterWhenHit.cs
+++ Assets/Scripts/Reactor/ShatterWhenHit.cs
@@ -29,4 +29,9 @@ public class ShatterWhenHit : MonoBehaviour {
 			}
 		}
 	}
+
+    public int getHPInternal()
+    {
+        return hpInternal;
+    }
 }
---
+++ Assets/Scripts/Shield and HUD/DoorHPDisplayController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DoorHPDisplayController : MonoBehaviour {
+
+    public ShatterWhenHit doorController;
+    private Image doorHPIndicator;
+    private HUDColorController colorController;
+
+	// Use this for initialization
+	void Start () {
+        doorHPIndicator = gameObject.GetComponent<Image>();
+        colorController = GameObject.FindGameObjectWithTag("GameController").GetComponent<HUDColorController>();
+
+        doorHPIndicator.color = colorController.getColorByString("bomb");
+        doorHPIndicator.fillAmount = (float)doorController.getHPInternal() / 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(doorController != null && doorController.getHPInternal() <= 0)
+        {
+            doorHPIndicator.enabled = false;
+        }
+	}
+
+    public void colorUpdate()
+    {
+        doorHPIndicator.color = colorController.getColorByString("bomb");
+    }
+}
