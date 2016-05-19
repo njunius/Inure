e5dc854453c9606b71b017201d5fd4aa5dfb7df8
Nick Junius <njunius@ucsc.edu>
2016-05-18 23:58:55 -0700

    Keybinding Screen QoL
    The player can no longer close the keybinding screen while binding a key
    by hitting the Escape key in game.

---
+++ Assets/Scripts/Menus/SettingsExitBuffer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingsExitBuffer : MonoBehaviour {
+
+    private bool keyBindingSelected;
+
+	// Use this for initialization
+	void Awake () {
+        keyBindingSelected = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public bool keyBindingIsSelected()
+    {
+        return keyBindingSelected;
+    }
+
+    public void setSelected(bool isSelected)
+    {
+        keyBindingSelected = isSelected;
+    }
+}
