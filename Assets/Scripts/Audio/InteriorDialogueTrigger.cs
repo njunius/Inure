a20a78d030a00042e4579501b75080440707e1a9
Joe Rossi <joethered@gmail.com>
2016-05-22 15:15:36 -0700

    Added Interior Dialogue

---
+++ Assets/Scripts/Audio/InteriorDialogueTrigger.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteriorDialogueTrigger : MonoBehaviour {
+    public AudioClip audioClip;
+    private AudioSource source;
+    private bool hasBeenPlayed = false;
+
+	// Use this for initialization
+	void Start () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (!hasBeenPlayed && other.CompareTag("Player Turret Trigger"))
+        {
+            source.Play();
+            hasBeenPlayed = true;
+        }
+    }
+}
