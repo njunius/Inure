34b2513ce267c68771140f99e7ddd028f7346330
JosephBernay <josephbernay@gmail.com>
2016-05-26 12:57:30 -0700

    Officially Added Core Visuals to Master
    I had to copy and paste backup versions of the scripts that I changed
    because Git completely got rid of my changes in them when I merged.
    
    There is now a deactivated turret that I will be using for future
    testing of this room that will not affect anything as long as it remains
    untouched.

--- Assets/reactorBomb.cs
+++ Assets/reactorBomb.cs
@@ -3,25 +3,44 @@ using System.Collections;
 
 public class reactorBomb : MonoBehaviour {
 	public GameObject escapeDoor;
+
+	private bool wasBombed = false;
+	private bool isNewColor = false;
+	private ParticleSystem reactionParticles;
+	private float reactionPulseInterval = 5f;
+
 	// Use this for initialization
 	void Start () {
-	
+		reactionParticles = transform.FindChild ("Bomb Particles").gameObject.GetComponent<ParticleSystem> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (wasBombed) {
+			if (!isNewColor) {
+				transform.FindChild ("ReactorDetailBomb").gameObject.GetComponent<MeshRenderer> ().enabled = true;
+				GetComponent<MeshRenderer> ().enabled = false;
+				reactionParticles.gameObject.SetActive (true);
+				Invoke ("BreakDoor", 1f);
+				isNewColor = true;
+			}
+		}
 	}
 
 	//when the player shoots the thing, and if the bomb's fully charged, do the other thing
 	void OnTriggerEnter(Collider other) {
 		if(other.tag == "Bomb Explosion"){
-			if(other.gameObject.GetComponent<PowerBombExplosion> ().GetPowerLevel () >= 100) {
+			if(other.gameObject.GetComponent<PowerBombExplosion> ().GetPowerLevel () >= 100 && !wasBombed) {
+				wasBombed = true;
 				GameObject.FindGameObjectWithTag("Bomb").GetComponent<BombDetach>().detached = true;
-				escapeDoor.GetComponent<ShatterWhenHit>().hitPoints = 0;
 				//hpInternal--;
 				//Destroy(collision.gameObject);
 			}
 		}
+	}
+
+	void BreakDoor() {
+		escapeDoor.GetComponent<ShatterWhenHit>().enabled = true;
+		escapeDoor.GetComponent<ShatterWhenHit>().hitPoints = 0;
 	}
 }
