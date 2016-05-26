8e2b22b7f1d41658357417f5f57a7a1421a238be
Yasha Taylor <YashaTaylor@gmail.com>
2016-05-25 18:39:48 -0700

    Added some lights to help players know where to go to exit the initial carrier, edited tunnel to interior

--- Assets/Scripts/TutorialEventManager.cs
+++ Assets/Scripts/TutorialEventManager.cs
@@ -342,7 +342,7 @@ public class TutorialEventManager : MonoBehaviour {
                 subtitles.enabled = true;
                 timer = 10;
                 //source.PlayOneShot(ShieldReady);
-                subtitles.text = "Hold [" + im.getPosInputName("Launch Bomb").ToUpper() + "] to charge bomb and release to launch.";
+                subtitles.text = "Hold [" + im.getPosInputName("Launch Bomb").ToUpper() + "] to charge bomb and release to launch. The charge required is always displayed on the door.";
                 break;
                 
 
