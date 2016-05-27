e35a133cfa1819678b5e5a28350cf06c4be0f3a0
Nick Junius <njunius@ucsc.edu>
2016-05-27 00:37:31 -0700

    Menu Background Animation
    Added a looping video to the main menu for the background.
    
    Note: The pngs I have are different from the rendered videos on the
    drive.
    
    Note: Unity can only properly accept .mov or .ogg files from what I've
    seen.
    
    NOTE: DO NOT BREAK THE MAIN MENU OR I WILL BREAK YOU (the scaling
    specifically).

---
+++ Assets/Scripts/Menus/MainMenuBackgroundController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainMenuBackgroundController : MonoBehaviour {
+
+    public MovieTexture menuBackground;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<RawImage>().texture = menuBackground as MovieTexture;
+        menuBackground.loop = true;
+        menuBackground.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+}
