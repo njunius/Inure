d8785d9dbd61b9f7415e6ec0cf65a921e7fd60b5
Nick Junius <njunius@ucsc.edu>
2016-05-31 22:49:17 -0700

    Added Fades Between Scenes
    The game now fades to black when loading scenes.
    
    Note: The endings still need fade ins and outs and getting to the dead
    ending doesn't have a fade out yet.

--- Assets/Scripts/Menus/ButtonLoadScene.cs
+++ Assets/Scripts/Menus/ButtonLoadScene.cs
@@ -7,13 +7,39 @@ using System.Collections;
 public class ButtonLoadScene : MonoBehaviour, IPointerClickHandler {
 
     public string sceneToLoad;
+    private CanvasGroup sceneTransition;
+
+    private bool startTransition;
+    private int transitionRate;
+    private float transitionCounter;
 
 	// Use this for initialization
 	void Start () {
-	
+
+        sceneTransition = GameObject.FindGameObjectWithTag("Screen Transition").GetComponent<CanvasGroup>();
+
+        startTransition = false;
+        transitionRate = 3;
+        transitionCounter = 0;
 	}
+
+    void Update()
+    {
+        if (startTransition)
+        {
+            transitionCounter += transitionRate * Time.unscaledDeltaTime;
+
+            sceneTransition.alpha = transitionCounter;
+        }
+
+        if(sceneTransition.alpha >= 1 && startTransition)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
 	
 	public void OnPointerClick(PointerEventData eventData) {
-        SceneManager.LoadScene(sceneToLoad);
+        startTransition = true;
+        Time.timeScale = 1;
 	}
 }
---
+++ Assets/Scripts/Menus/ScreenTransitionFadeController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenTransitionFadeController : MonoBehaviour {
+
+    private CanvasGroup sceneTransition;
+
+    private bool startTransition;
+    private int transitionRate;
+    private float transitionCounter;
+
+    // Use this for initialization
+    void Start () {
+
+        sceneTransition = GetComponent<CanvasGroup>();
+
+        startTransition = true;
+        transitionRate = 3;
+        transitionCounter = 1;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (startTransition)
+        {
+            transitionCounter -= transitionRate * Time.unscaledDeltaTime;
+
+            sceneTransition.alpha = transitionCounter;
+        }
+    }
+}
