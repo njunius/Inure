3bd294f6122f80bed7b1ece7e10982b2f0cf38d5
Nick Junius <njunius@ucsc.edu>
2016-06-02 03:08:15 -0700

    Credits Update
    Credits can now be accessed from the main menu and endings.
    
    Players can exit them by pressing escape or space.

---
+++ Assets/Scripts/Menus/CreditsToMainMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CreditsToMainMenu : MonoBehaviour {
+
+    private CanvasGroup sceneTransition;
+
+    private bool startTransition;
+    private int transitionRate;
+    private float transitionCounter;
+    
+    // Use this for initialization
+    void Start () {
+        sceneTransition = GameObject.FindGameObjectWithTag("Screen Transition").GetComponent<CanvasGroup>();
+
+        startTransition = false;
+        transitionRate = 3;
+        transitionCounter = 0;
+        AudioListener.pause = false;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Space))
+        {
+            startTransition = true;
+            AudioListener.pause = true;
+
+        }
+
+        if (transitionCounter >= 1)
+        {
+            SceneManager.LoadScene("00-MainMenu");
+        }
+
+        if (startTransition)
+        {
+            transitionCounter += transitionRate * Time.unscaledDeltaTime;
+
+            sceneTransition.alpha = transitionCounter;
+        }
+    }
+
+    public void endCredits()
+    {
+        AudioListener.pause = true;
+        startTransition = true;
+    }
+}
