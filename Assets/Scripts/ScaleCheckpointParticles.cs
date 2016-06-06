b0183340d120656873b7b21a05a36977271ccda8
JosephBernay <josephbernay@gmail.com>
2016-06-05 18:04:25 -0700

    Fixed Checkpoint Particle System Scaling
    The checkpoint particle effects were not correctly automatically scaling
    their startLifetime to the scaling of the object itself due to some
    shifting around of the particle system inside the prefab.
    
    This was fixed.

--- Assets/Scripts/ScaleCheckpointParticles.cs
+++ Assets/Scripts/ScaleCheckpointParticles.cs
@@ -5,7 +5,7 @@ public class ScaleCheckpointParticles : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		gameObject.GetComponent<ParticleSystem> ().startLifetime *= transform.parent.localScale.z;
+		gameObject.GetComponent<ParticleSystem> ().startLifetime *= transform.parent.parent.localScale.z;
 	}
 	
 	// Update is called once per frame
